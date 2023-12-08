a71fa7e8a2dc5f0a40bf9d580a96d413ed943a76
Eva Čechová <cechovaev@gmail.com>
2023-12-08 23:59:30 +0100

    basic-structure-and-texts

---
+++ .eleventy.js
@@ -0,0 +1,21 @@
+module.exports = function(eleventyConfig) {
+
+    eleventyConfig.setServerOptions({
+        showAllHosts: true,
+      })
+      
+    // Výchozí výstupní složka je: _site
+    // Zkopírovat images/ do _site/images
+    eleventyConfig.addPassthroughCopy("images");
+    // Zkopírovat css/ to _site/css/
+    eleventyConfig.addPassthroughCopy("css");
+   };
+
+   return {
+    // možné formáty šablon
+    templateFormats: ["njk", "html", "md"],
+    // jako šablonovací jazyk zvolíme Nunjucks`
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+  }
